0a6da9f1b2e33a306ec42886333ef28ec803791b
daijianchen <719857499@qq.com>
2023-10-07 08:59:43 +0800

    34

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vitePluginMd from './src/plugins/vueMdPlugin'
+import VueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue({
       include: [/(\.vue)$/, /\.md$/]
     }),
